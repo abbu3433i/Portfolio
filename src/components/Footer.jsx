bfe87ca729201dcb597664cba592412a76aac98f
unknown <you@example.com>
2024-08-09 23:17:36 +0530

    code

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,11 +9,20 @@ function Footer() {
         <div className='max-w-screen-2xl mx-auto container px-4 md:px-20 my-20'>
             <div className='flex flex-col justify-center items-center'>
                 <div className='flex space-x-4'>
-                    <FaFacebook size={26}/>
-                    <FaTwitter size={26}/>
-                    <FaInstagram size={26}/>
-                    <FaLinkedinIn size={26}/>
+                    <a href="https://www.facebook.com/login/" target='blank'>
+                        <FaFacebook size={26}/>
+                    </a>
+                    <a href="https://x.com/?lang=en" target='blank'>
+                        <FaTwitter size={26}/>             
+                    </a>
+                    <a href="https://www.instagram.com/" target='blank'>
+                        <FaInstagram size={26}/>
+                    </a>
+                    <a href="https://www.linkedin.com/in/abhishek-gour-248703258/" target='blank'>
+                        <FaLinkedinIn size={26}/>
+                    </a>   
                 </div>
+
                 <div className='mt-8 border-t border-gray-700 pt-8 flex flex-col items-center'> {/*border-t is responsible for create small horizontal line */}
                     <p className='text-sm'>&copy; 2024 Company. All right reserved.</p>
                     <p className='text-sm'>Supportive partner ❤️Gopal </p>
